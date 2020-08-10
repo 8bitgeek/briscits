b6b431bf8be831fbb69d3e9f3cd7a4f41b39659a
8bitgeek <mike@8bitgeek.net>
2020-08-10 04:25:37 -0400

    fixed mutex

--- thread/brisc_mutex.c
+++ thread/brisc_mutex.c
@@ -4,21 +4,21 @@
 
 extern void b_mutex_lock( brisc_mutex_t* mutex )
 {
-    int state = b_int_enabled();
-    b_int_disable();
-    while ( b_mutex_try_lock(mutex) )
-    {
-        b_int_enable();
-        b_thread_yield();
-        b_int_disable();
-    }
-    *mutex = 1;
-    b_int_set(state);
+    b_thread_block_while( !b_mutex_try_lock(mutex) ); 
 }
 
 extern bool b_mutex_try_lock( brisc_mutex_t* mutex )
 {
-    return *mutex;
+    int state = b_int_enabled();
+    b_int_disable();
+    if ( !*mutex )
+    {
+        *mutex = 1;
+        b_int_set(state);
+        return true;
+    }
+    b_int_set(state);
+    return false;
 }
 
 extern void b_mutex_unlock( brisc_mutex_t* mutex )
