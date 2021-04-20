dabd03d1b097058f0f00d2a70382031dd9d74e0b
8bitgeek <mike@8bitgeek.net>
2021-04-19 22:32:48 -0400

    Merge branch 'cortex-m7-target'

--- src/brisc_irq.c
+++ src/brisc_irq.c
@@ -1,12 +1,45 @@
+/******************************************************************************
+ ________  ________  ___  ________  ________  ___  _________  ________      
+|\   __  \|\   __  \|\  \|\   ____\|\   ____\|\  \|\___   ___\\   ____\     
+\ \  \|\ /\ \  \|\  \ \  \ \  \___|\ \  \___|\ \  \|___ \  \_\ \  \___|_    
+ \ \   __  \ \   _  _\ \  \ \_____  \ \  \    \ \  \   \ \  \ \ \_____  \   
+  \ \  \|\  \ \  \\  \\ \  \|____|\  \ \  \____\ \  \   \ \  \ \|____|\  \  
+   \ \_______\ \__\\ _\\ \__\____\_\  \ \_______\ \__\   \ \__\  ____\_\  \ 
+    \|_______|\|__|\|__|\|__|\_________\|_______|\|__|    \|__| |\_________\
+                            \|_________|                        \|_________|
+                            
+MIT License
+
+Copyright (c) 2021 Mike Sharkey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+******************************************************************************/
 #include <brisc_irq.h>
 #include <brisc_sched.h>
 
-#define thread_scheduler_service()  register cpu_reg_t context_sp;                      \
-                                    if ( ( context_sp = thread_schedule_next() ) != 0 ) \
+#define thread_scheduler_service()  register cpu_reg_t context_sp;          \
+                        if ( ( context_sp = thread_schedule_next() ) != 0 ) \
                                         cpu_wr_sp( context_sp )
 
-#define systick_service()           ++brisc_scheduler_state.systick;                          \
-                                    cpu_systick_clear()
+#define systick_service() ++brisc_scheduler_state.systick;                  \
+                        cpu_systick_clear()
 
 /**
  * @brief timer interrupt, increment systick, and potentially switch thread context
--- src/brisc_thread.c
+++ src/brisc_thread.c
@@ -1,10 +1,39 @@
+/******************************************************************************
+ ________  ________  ___  ________  ________  ___  _________  ________      
+|\   __  \|\   __  \|\  \|\   ____\|\   ____\|\  \|\___   ___\\   ____\     
+\ \  \|\ /\ \  \|\  \ \  \ \  \___|\ \  \___|\ \  \|___ \  \_\ \  \___|_    
+ \ \   __  \ \   _  _\ \  \ \_____  \ \  \    \ \  \   \ \  \ \ \_____  \   
+  \ \  \|\  \ \  \\  \\ \  \|____|\  \ \  \____\ \  \   \ \  \ \|____|\  \  
+   \ \_______\ \__\\ _\\ \__\____\_\  \ \_______\ \__\   \ \__\  ____\_\  \ 
+    \|_______|\|__|\|__|\|__|\_________\|_______|\|__|    \|__| |\_________\
+                            \|_________|                        \|_________|
+                            
+MIT License
+
+Copyright (c) 2021 Mike Sharkey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+******************************************************************************/
 #include <brisc_thread.h>
 #include <brisc_sched.h>
 #include <string.h>
-
-#define thread_msip_set()           *( volatile uint8_t * )( TIMER_CTRL_ADDR + TIMER_MSIP ) = 0x01
-#define thread_lock()               (++brisc_scheduler_state.lock)
-#define thread_unlock()             (--brisc_scheduler_state.lock)
 
 static int       thread_new_id( void );
 static void      thread_exit  ( void );
@@ -40,23 +69,21 @@ void b_thread_start(int id)
 
 void b_thread_lock( void )
 {
-    thread_lock();
+    ++brisc_scheduler_state.lock
 }
 
 void b_thread_unlock( void )
 {
-    thread_unlock();
+    --brisc_scheduler_state.lock
 }
 
 void b_thread_yield( void )
 {
-    /* trigger the timer "soft" interrupt */
-    thread_msip_set();
+    cpu_yield();
 }
 
 static void thread_exit( void )
 {
-    // TODO - de-allocate, clean up
     // wait to die
     for(;;);
 }
