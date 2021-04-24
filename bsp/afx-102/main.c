90dd6722ac5e30edded37e2863283c546b389d88
8bitgeek <mike@8bitgeek.net>
2021-04-24 13:34:20 -0400

    removed unused const from cortex-m7 test

--- bsp/afx-102/main.c
+++ bsp/afx-102/main.c
@@ -1,6 +1,6 @@
 #include <brisc_thread.h>
 #include <brisc_delay.h>
-#include <xprintf.h>
+#include <string.h>
 
 #define DEC_DELAY       (2)
 #define INC_DELAY       DEC_DELAY
@@ -20,36 +20,26 @@ static int green_thread_handle = (-1);
 static int blue_thread_handle  = (-1);
 static int main_thread_handle  = (-1);
 
-static void sweep_delay(int* delay);
+typedef struct _app_state_ {
+    int red_count;
+    int green_count;
+    int blue_count;
+    int main_count;
+} app_state_t;
+
+app_state_t app_state;
 
 static void run_red  (void* arg);
 static void run_green(void* arg);
 static void run_blue (void* arg);
 static void run_main (void* arg);
 
-static void sweep_delay(int* delay)
-{
-    static bool dir=false;
-
-    if ( dir )
-    {
-        *delay = *delay + INC_DELAY;
-        if ( *delay >= MAX_DELAY )
-            dir = !dir;
-    }
-    if ( !dir )
-    {
-        *delay = *delay - DEC_DELAY;
-        if ( *delay <= MIN_DELAY )
-            dir = !dir;
-    }
-} 
-
-
 static void run_red(void* arg)
 {
     for(EVER)
     {
+        ++app_state.red_count;
+        b_thread_yield();
     }
 }
 
@@ -57,6 +47,8 @@ static void run_green(void* arg)
 {
     for(EVER)
     {
+        ++app_state.green_count;
+        b_thread_yield();
     }
 }
 
@@ -64,6 +56,8 @@ static void run_blue(void* arg)
 {
     for(EVER)
     {
+        ++app_state.blue_count;
+        b_thread_yield();
     }
 }
 
@@ -71,39 +65,32 @@ static void run_blue(void* arg)
 // 'main' thread - sweeps the delay
 static void run_main(void* arg)
 {
-    int* delay = (int*)arg;
     for(EVER)
     {
-        sweep_delay(delay);
-        b_delay_ms((*delay)*2);
+        ++app_state.main_count;
+        b_thread_yield();
     }
 }
 
 
 int main( void )
 {
-    int delay = MAX_DELAY;
-
-    xprintf( "CLK = %f GHz\n", (float)board_clkfreq()/1000000000.0f );
-
+    memset(&app_state,0,sizeof(app_state_t));
     if ( (main_thread_handle  = b_thread_init( "main" )) >= 0 )
     {
-        if ( (red_thread_handle = b_thread_create( "red", run_red, &delay, red_stack, STACK_WORDS )) >= 0)
+        if ( (red_thread_handle = b_thread_create( "red", run_red, NULL, red_stack, STACK_WORDS )) >= 0)
         {
-            if ( (green_thread_handle = b_thread_create( "green", run_green, &delay, green_stack, STACK_WORDS )) >= 0)
+            if ( (green_thread_handle = b_thread_create( "green", run_green, NULL, green_stack, STACK_WORDS )) >= 0)
             {
-                if ( (blue_thread_handle = b_thread_create( "blue", run_blue, &delay, blue_stack, STACK_WORDS )) >= 0)
+                if ( (blue_thread_handle = b_thread_create( "blue", run_blue, NULL, blue_stack, STACK_WORDS )) >= 0)
                 {
                     b_thread_start( red_thread_handle );
                     b_thread_start( blue_thread_handle );
                     b_thread_start( green_thread_handle );
-                    run_main( &delay );
+                    run_main( NULL );
                 }
             }
         }
     }
-    
-    xprintf( "failed to create a thread\n\n" );
-
     return 0;
 }
